c36e19eee8aa0e403007e28e47ec36e92e90db19
Gerardo Rodrigues <gerardofilho56@gmail.com>
2022-01-17 17:10:00 -0300

    Reposity e Service para Categoria

--- src/main/java/com/example/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -3,26 +3,34 @@ package com.example.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
+	@GetMapping
+	public List<Categoria> buscarTodas(){
+		List<Categoria> lista = new ArrayList<>();
+		lista.addAll(service.buscarTudo());
 		
-		List<Categoria> list = new ArrayList<>();
-		
-		list.add(cat1);
-		list.add(cat2);
-		
-		return list;
+		return lista;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/example/cursomc/services/CategoriaService.java
@@ -0,0 +1,30 @@
+package com.example.cursomc.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+	public List<Categoria> buscarTudo(){
+		List<Categoria> lista = new ArrayList<>();
+		lista.addAll(repo.findAll());
+		
+		return lista;
+	}
+}
