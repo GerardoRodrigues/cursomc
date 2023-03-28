0522852e7841403db98cbb05a2be688ad039d579
Gerardo Rodrigues <gerardofilho56@gmail.com>
2023-03-28 14:48:28 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/example/cursomc/resources/PedidoResource.java
+++ src/main/java/com/example/cursomc/resources/PedidoResource.java
@@ -1,18 +1,18 @@
 package com.example.cursomc.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,12 +26,12 @@ public class PedidoResource {
 	@Autowired
 	private PedidoService service;
 	
-	@GetMapping
-	public List<Pedido> buscarTudo(){
-		List<Pedido> lista = new ArrayList<>();
-		lista.addAll(service.buscarTudo());
-		return lista;
-	}
+//	@GetMapping
+//	public List<Pedido> buscarTudo(){
+//		List<Pedido> lista = new ArrayList<>();
+//		lista.addAll(service.buscarTudo());
+//		return lista;
+//	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Pedido> buscarPorID(@PathVariable Integer id){
@@ -44,5 +44,15 @@ public class PedidoResource {
 		obj = service.inserir(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<Pedido>> buscarPagina(@RequestParam(value = "pagina", defaultValue = "0") Integer pagina,
+			@RequestParam(value = "linhasPorPagina", defaultValue = "24") Integer linhasPorPagina,
+			@RequestParam(value = "ordem", defaultValue = "instante") String ordem,
+			@RequestParam(value = "direcao", defaultValue = "DESC") String direcao){
+			
+		Page<Pedido> page = service.findPage(pagina, linhasPorPagina, ordem, direcao);
+		return ResponseEntity.ok().body(page);
 	}
 }
