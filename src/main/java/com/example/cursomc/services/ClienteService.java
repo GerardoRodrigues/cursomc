016b01f34a3915df10b0364da5d20b87d0bd902d
Gerardo Rodrigues <gerardofilho56@gmail.com>
2023-04-14 15:07:49 -0300

    Salvando URL da imagem em Cliente

--- src/main/java/com/example/cursomc/services/ClienteService.java
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -116,6 +116,17 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3service.uploadFile(multipartFile);
+		UserSS user = UserService.autheticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		URI uri =  s3service.uploadFile(multipartFile);
+		
+		Optional<Cliente> cli = repo.findById(user.getId());
+		cli.orElse(null).setImageUrl(uri.toString());
+		repo.save(cli.orElse(null));
+		
+		return uri;
 	}
 }
